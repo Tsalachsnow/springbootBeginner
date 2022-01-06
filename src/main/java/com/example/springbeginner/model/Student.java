63829a867908e872674ceee996c58a6b4f250edc
TsalachSnow <prosper.amalaha@decagon.dev>
2022-01-06 23:39:01 +0100

    creating the model class for this project

--- src/main/java/com/example/springbeginner/model/Student.java
+++ src/main/java/com/example/springbeginner/model/Student.java
@@ -1,14 +1,29 @@
 package com.example.springbeginner.model;
 
 import java.time.LocalDate;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+@Entity
+@Table(name = "student")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private int id;
     private String name;
     private String email;
@@ -32,4 +47,7 @@ public class Student {
                 ", dob=" + dob +
                 '}';
     }
+
+
+
 }
